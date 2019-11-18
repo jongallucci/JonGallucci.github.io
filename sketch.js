c56d571cf925d6d5db25cb96926592a33c0db247
Jonathon Gallucci <JonathonGallucci@gmail.com>
2019-11-17 19:33:37 -0500

    website redesigned to look like man page with information about myself

--- sketch.js
+++ sketch.js
@@ -1,54 +1,92 @@
-let fontRobotoMono;
+var fontRobotoMono;
 
+// load font
 function preload() {
   fontRobotoMono = loadFont('RobotoMono-Regular.ttf');
 }
 
-let string = "  welcome  ";
-let correctChars = [];
+var salutation = "  welcome  ";
+var correctChars = [];
 
+// create canvas and set basic fields
 function setup() {
-  createCanvas(windowWidth - 15, 400);
+  var canvas = createCanvas(windowWidth - 15, windowHeight / 2);
   background(51);
-  frameRate(9);
+  frameRate(8);
+  canvas.parent("welcome-sketch");
 
-  for (let i = 0; i < string.length; i++) {
+  // make a bool array for 
+  for (var i = 0; i < salutation.length; i++) {
     correctChars.push(false);
   }
 }
 
+// used as cut off for unscrammbling salutation and begin printing intro
+var counter = 0;
+var numLettersToDisplay = 1;
 function draw() {
-  background(51);
+  background(40);
 
   chanceToCorrectChars();
-  let randStr = randString(string);
+  var randStr = randString(salutation);
+
+  // basic settings
   textFont(fontRobotoMono);
-  textSize(46);
+  textSize(windowWidth / 14);
   textAlign(CENTER);
   fill(90, 200, 62);
   text(randStr, width / 2, height / 2);
+
+  if (counter > 30) {
+
+    // blinky effect of cursor
+    var intro = "to my manpage ";
+    if (counter % 4 == 0)
+      intro = "to my manpage_";
+
+    // prints out intro string
+    if (counter < intro.length + 30) {
+      whitespace = "              ".substring(0, intro.length - numLettersToDisplay - 1)
+      intro = intro.substring(0, numLettersToDisplay)
+      intro = intro + "_" + whitespace
+      numLettersToDisplay++;
+    }
+
+
+    textSize(windowWidth / 20);
+    text(intro, width / 2, height / 2 + windowWidth / 14);
+  }
 }
 
+// provides random chars
 function randString(str) {
-  let randStr = [];
-  for (let i = 0; i < str.length; i++) { // 33 -> 126
+  var randStr = [];
+  for (var i = 0; i < str.length; i++) {
     randStr.push(String.fromCharCode(round(random(64, 126))));
   }
   return correctCharsInString(randStr);
 }
 
+// corrects random chars in salutation string
 function correctCharsInString(randStr) {
-  for (let i = 0; i < randStr.length; i++) {
-    if (correctChars[i])
-      randStr[i] = string[i];
+  counter++;
+  for (var i = 0; i < randStr.length; i++) {
+    if (correctChars[i] || counter > 28)
+      randStr[i] = salutation[i];
   }
   return (randStr.toString()).replace(/,/g, '');
 }
 
+// has a chance to turn into correct char instead of random char
 function chanceToCorrectChars() {
-  for (let i = 0; i < correctChars.length; i++) {
-    if (random(1) < 0.065) {
+  for (var i = 0; i < correctChars.length; i++) {
+    if (random(1) < 0.08) {
       correctChars[i] = true;
     }
   }
+}
+
+// Resizes the canvas window
+function windowResized() {
+  resizeCanvas(windowWidth - 15, windowHeight / 2);
 }
